9a500f73ff0ea3a617a8c0281e70d49f9fc61ae6
SAAH THIBAUT <saahthibaut@gmail.com>
2018-10-29 18:55:29 +0100

    editing plot2.R

--- plot2.R
+++ plot2.R
@@ -1,20 +1,21 @@
-Sys.setlocale("LC_TIME","English")
+#Sys.setlocale("LC_TIME","English")
 #Reading data
 data<-read.csv("household_power_consumption.txt", sep=';', na.strings="?",
                check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 
+data$Date<-as.Date(data$Date, format="%d/%m/%Y")
 #Get an idea on data structure
 str(data)
 #Selecting of data where Date is in 1/2/2007 or 2/2/2007
-energyData <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+energyData<- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
 rm(data)
 #Convert Date in date class
-Date <- as.Date(Date, format="%d/%m/%Y")
-datetime <- paste(as.Date(Data1$Date), Data1$Time)
-energyData$Datetime <- as.POSIXct(datetime)
-attach(energyData)
+datetime <- paste(as.Date(energyData$Date), energyData$Time)
+energyData$datetime <- as.POSIXct(datetime)
+
 #Plotting time-series with Date labels on x-axis, Global_active_power in y-axis 
-plot(Global_active_power~Datetime, type="l",
+plot(energyData$Global_active_power~energyData$datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
 #Saving plot2
 dev.copy(png, file="plot2.png", height=480, width=480)
